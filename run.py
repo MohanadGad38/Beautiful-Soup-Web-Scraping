45f6518f23da064d0b73ba9022900bddb6a7731d
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2024-04-01 09:55:56 +0200

    lst of top 10 action movies

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open('home.html','r') as html_file:
-    content=html_file.read()
 
-    soup=BeautifulSoup(content,'lxml')
-    tags=soup.find('h5')
-    print(tags)
+
+htmlT=requests.get('https://elcinema.com/en/lineup/9990411/').text
+# print(htmlT)
+# with open('home.html','r') as html_file:
+#     content=html_file.read()
+
+soup=BeautifulSoup(htmlT,'lxml')
+top10ActionMovies=soup.find_all('ul',class_='unstyled')
+
+for c in top10ActionMovies:
+    # print(c.h5.text)
+    print(c.a.text)
