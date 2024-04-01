73ef105d9904576f1df2bc1a55defc5e18db12ff
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2024-04-01 10:33:07 +0200

    filtering

--- run.py
+++ run.py
@@ -14,9 +14,19 @@ top10ActionMovies=soup.find_all('ul',class_='unstyled')
 for c in top10ActionMovies:
     print(c.a.text) #movie name 
     print(c.p.text) #description 
+    top10ActionMoviesRates=c.find('span',class_='legend')
+    print(top10ActionMoviesRates.text)  #movie rate
+    
 
 
-top10ActionMoviesRates=soup.find_all('span',class_='legend')
-for c in top10ActionMoviesRates:
-    print(c.text) #movie rtes 
+print("if rate >5")
+for c in top10ActionMovies:
+    top10ActionMoviesRates=c.find('span',class_='legend')
+    if(float(top10ActionMoviesRates.text)>5):
+        print(c.a.text) #movie name 
+        print(c.p.text) #description 
+        print(top10ActionMoviesRates.text)  #movie rate
+# top10ActionMoviesRates=soup.find_all('span',class_='legend')
+# for c in top10ActionMoviesRates:
+#     print(c.text) #movie rtes 
 
