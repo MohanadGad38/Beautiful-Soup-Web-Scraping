c51ec05e6ae78e8820d4f8366f9e354e48839d04
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2024-04-01 10:50:41 +0200

    into json file

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-
+import json
 
 
 htmlT=requests.get('https://elcinema.com/en/lineup/9990411/').text
@@ -10,15 +10,23 @@ htmlT=requests.get('https://elcinema.com/en/lineup/9990411/').text
 
 soup=BeautifulSoup(htmlT,'lxml')
 top10ActionMovies=soup.find_all('ul',class_='unstyled')
-
+movies:list=[]
 for c in top10ActionMovies:
-    print(c.a.text) #movie name 
-    print(c.p.text) #description 
+    # print(c.a.text) #movie name 
+    # print(c.p.text) #description 
     top10ActionMoviesRates=c.find('span',class_='legend')
-    print(top10ActionMoviesRates.text)  #movie rate
-    
+    # print(top10ActionMoviesRates.text)  #movie rate
+    obj = {
+            "Movie  Name": c.a.text,
+            "description ": c.p.text,
+            "Rate":top10ActionMoviesRates.text
+
+    }
+    movies.append(obj)
 
 
+with open('top10.jsonl', 'w') as f:
+    json.dump(movies, f)
 print("if rate >5")
 for c in top10ActionMovies:
     top10ActionMoviesRates=c.find('span',class_='legend')
