a96725001f69e1cf1521f940f9bd146e10a9296f
MohanadGad38 <mohanad.gad38@gmail.com>
2024-05-09 11:35:02 +0300

    clean code

--- run.py
+++ run.py
@@ -3,23 +3,23 @@ import requests
 import json
 
 
-htmlT=requests.get('https://elcinema.com/en/lineup/9990411/').text
+htmlT = requests.get('https://elcinema.com/en/lineup/9990411/').text
 # print(htmlT)
 # with open('home.html','r') as html_file:
 #     content=html_file.read()
 
-soup=BeautifulSoup(htmlT,'lxml')
-top10ActionMovies=soup.find_all('ul',class_='unstyled')
-movies:list=[]
+soup = BeautifulSoup(htmlT, 'lxml')
+top10ActionMovies = soup.find_all('ul', class_='unstyled')
+movies: list = []
 for c in top10ActionMovies:
-    # print(c.a.text) #movie name 
-    # print(c.p.text) #description 
-    top10ActionMoviesRates=c.find('span',class_='legend')
+    # print(c.a.text) #movie name
+    # print(c.p.text) #description
+    top10ActionMoviesRates = c.find('span', class_='legend')
     # print(top10ActionMoviesRates.text)  #movie rate
     obj = {
-            "Movie  Name": c.a.text,
-            "description ": c.p.text,
-            "Rate":top10ActionMoviesRates.text
+        "Movie  Name": c.a.text,
+        "description ": c.p.text,
+        "Rate": top10ActionMoviesRates.text
 
     }
     movies.append(obj)
@@ -27,25 +27,24 @@ for c in top10ActionMovies:
 
 with open('top10.jsonl', 'w') as f:
     json.dump(movies, f)
-movies=[]
+movies = []
 print("if rate >5")
 for c in top10ActionMovies:
-    top10ActionMoviesRates=c.find('span',class_='legend')
-    if(float(top10ActionMoviesRates.text)>5):
-        # print(c.a.text) #movie name 
-        # print(c.p.text) #description 
+    top10ActionMoviesRates = c.find('span', class_='legend')
+    if (float(top10ActionMoviesRates.text) > 5):
+        # print(c.a.text) #movie name
+        # print(c.p.text) #description
         # print(top10ActionMoviesRates.text)  #movie rate
         # print(top10ActionMoviesRates.text)  #movie rate
         obj = {
             "Movie  Name": c.a.text,
             "description ": c.p.text,
-            "Rate":top10ActionMoviesRates.text
+            "Rate": top10ActionMoviesRates.text
 
-    }
+        }
     movies.append(obj)
 with open('top10if-rate>5.jsonl', 'w') as f:
     json.dump(movies, f)
 # top10ActionMoviesRates=soup.find_all('span',class_='legend')
 # for c in top10ActionMoviesRates:
-#     print(c.text) #movie rtes 
-
+#     print(c.text) #movie rtes
